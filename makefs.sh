d12e7ffaf8e9d5a5d01241c24bcd99326dcc72e7
crocogene <crocogene@yandex.com>
2025-09-01 23:34:42 +0300

    fixes

--- makefs.sh
+++ makefs.sh
@@ -6,6 +6,6 @@ mkfs.vfat -n $esp_label -F 32 $esp_part
 mkfs.btrfs $tank_part -L $tank_label --force
 mount -o $btrfs_opt /dev/disk/by-label/$tank_label /mnt
 cd /mnt
-btrfs subvol create {@,@home,@cache,@swap,@shapshots}  
+btrfs subvol create {@,@home,@cache,@swap,@snapshots}  
 cd ..
 umount /mnt
