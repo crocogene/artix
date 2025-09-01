79da10f6d5a529a3d6cd28ece0a3fd1800a00daf
crocogene <crocogene@yandex.com>
2025-09-02 00:00:50 +0300

    keyring

--- basestrap.sh
+++ basestrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-basestrap -UD /mnt 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+basestrap -UD /mnt \
+'https://packages.artixlinux.org/packages/system/any/artix-keyring/download/' \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
