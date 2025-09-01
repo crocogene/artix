3b2ecb4aa7802d3b924ff960ffb50776e8ed2a3b
crocogene <crocogene@yandex.com>
2025-09-01 23:29:29 +0300

    fixes

--- opts.sh
+++ opts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-btrf_opt=compress-force=zstd:-1,noatime,commit=60
+btrfs_opt=compress-force=zstd:-1,noatime,commit=60
 esp_part=/dev/"$1"p1
 esp_label=ESP
 tank_part=/dev/"$1"p2
