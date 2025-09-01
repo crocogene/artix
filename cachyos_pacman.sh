b9aa6f6df6c788111ddee11cb6971aff9637656a
crocogene <crocogene@yandex.com>
2025-09-01 22:31:46 +0300

    cachyos pacman

--- cachyos_pacman.sh
+++ cachyos_pacman.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47 
-pacman -Uy 'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
+pacman -U 'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
