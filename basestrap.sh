1ee13f904fecca9636a54421068c66fb23139097
crocogene <crocogene@yandex.com>
2025-09-02 22:58:49 +0300

    grep-ugrep-shim, pacman.conf arch-custom-repo

--- basestrap.sh
+++ basestrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-basestrap -P /mnt base seatd-dinit cachyos-keyring cachyos-mirrorlist cachyos-v4-mirrorlist artix-archlinux-support
+basestrap -P /mnt \
+    system/base seatd-dinit \
+    cachyos-keyring cachyos-mirrorlist cachyos-v4-mirrorlist artix-archlinux-support \
+    gzip-pigz-shim
 fstabgen -U /mnt >> /mnt/etc/fstab
