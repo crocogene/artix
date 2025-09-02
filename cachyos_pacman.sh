379fbd9c13c68e0edba34cd83feb0a6b92a572a9
crocogene <crocogene@yandex.com>
2025-09-02 08:32:59 +0300

    cachyos pacman: clear cache

--- cachyos_pacman.sh
+++ cachyos_pacman.sh
@@ -3,3 +3,4 @@
 pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47 
 pacman -U --noconfirm 'https://mirror.cachyos.org/repo/x86_64/cachyos/pacman-7.0.0.r7.g1f38429-1-x86_64.pkg.tar.zst'
+pacman -Scc
