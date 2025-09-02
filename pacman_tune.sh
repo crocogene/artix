8c534f096e230841449e553f817e83655d101b86
crocogene <crocogene@yandex.com>
2025-09-02 14:24:45 +0300

    cachyos-mirrorlist

--- pacman_tune.sh
+++ pacman_tune.sh
@@ -5,6 +5,7 @@ pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47 
 pacman -U --noconfirm \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' 
 cp ~/artix/etc/pacman.conf /etc/pacman.conf
 pacman -Syu
